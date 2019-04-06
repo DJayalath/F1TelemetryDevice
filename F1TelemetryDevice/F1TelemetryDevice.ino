03ae112e77a4002bb23a2b95d199f24011fd4bec
Dulhan Jayalath <dhjayalath@gmail.com>
2019-04-06 14:51:05 +0100

    Add project files.

---
+++ F1TelemetryDevice/F1TelemetryDevice.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		F1TelemetryDevice.ino
+ Created:	4/6/2019 2:51:00 PM
+ Author:	dhjay
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
